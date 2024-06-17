2ad041acdae59f2056e937520be871c9e47789bc
KayeJin <pengjin250@gmail.com>
2024-06-17 01:51:58 +0000

    add extern in wbaes

--- include/wbaes.h
+++ include/wbaes.h
@@ -4,12 +4,18 @@
 #include "aes.h"
 #include "WBMatrix/WBMatrix.h"
 
-u32 TypeII[10][16][256];//Type II
-u32 TypeIII[9][16][256];//Type III
-u8 TypeIV_II[9][4][3][8][16][16];
-u8 TypeIV_III[9][4][3][8][16][16];
-u8 TypeIa[16][256];
-u8 TypeIb[16][256];
+extern u32 TypeII[10][16][256];//Type II
+extern u32 TypeIII[9][16][256];//Type III
+extern u8 TypeIV_II[9][4][3][8][16][16];
+extern u8 TypeIV_III[9][4][3][8][16][16];
+extern u8 TypeIa[16][256];
+extern u8 TypeIb[16][256];
+// u32 TypeII[10][16][256];//Type II
+// u32 TypeIII[9][16][256];//Type III
+// u8 TypeIV_II[9][4][3][8][16][16];
+// u8 TypeIV_III[9][4][3][8][16][16];
+// u8 TypeIa[16][256];
+// u8 TypeIb[16][256];
 
 void wbaes_gen(u8 key[16]);
 void wbaes_encrypt(u8 input[16], u8 output[16]);
